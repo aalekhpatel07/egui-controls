e4ae7a1cb15ec39ad69812034595f34061a1ddec
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-06-24 16:46:52 -0500

    Add examples.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,67 @@
 mod parse;
-mod ui;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
+
+
+/// # ControlPanel
+///
+/// Deriving ControlPanel on a struct generates a control
+/// panel widget (based on egui) that lets you interact with the fields
+/// of the struct.
+///
+/// This exposes a method `ui` on the underlying struct.
+///
+/// # Note:
+/// This can be especially useful if you're implementing
+/// an algorithm that has a bunch of tunable params and
+/// want to inspect the output by tweaking the parameters
+/// in real time.
+///
+/// # Examples
+/// ```no_run
+/// use egui_controls::ControlPanel;
+/// use serde::{Serialize, Deserialize};
+///
+/// #[derive(Debug, Clone, Serialize, Deserialize, ControlPanel)]
+/// pub struct CirclePackingAlgorithmConfig {
+///     /// The radius of the circles to pack.
+///     #[control(slider(2. ..= 15.0))]
+///     pub radius: f64,
+///     /// If circles overlap, then how many should be allowed
+///     /// to overlap at most.
+///     #[control(slider(0 ..= 20))]
+///     pub max_overlap_count: usize,
+///     #[control(textbox)]
+///     pub circle_label: String
+/// }
+///
+/// impl Default for CirclePackingAlgorithmConfig {
+/// fn default() -> Self {
+///     Self {
+///         radius: 12.0,
+///         max_overlap_count: 10,
+///         circle_label: "Some text".to_string()
+///     }
+/// }}
+///
+/// #[derive(Debug, Clone, Default)]
+/// pub struct MyApp {
+///     settings: CirclePackingAlgorithmConfig
+/// }
+///
+/// impl eframe::App for MyApp {
+///     fn update(&mut self, ctx: &::eframe::egui::Context, frame: &mut ::eframe::Frame) {
+///         ::eframe::egui::CentralPanel::default().show(ctx, |ui: &mut ::eframe::egui::Ui| {
+///             self.settings.ui(ui);
+///             ui.vertical(|ui| {
+///                 ui.code(format!("{:#?}", &self.settings));
+///             });
+///         });
+///     }
+/// }
+/// ```
 #[proc_macro_derive(ControlPanel, attributes(control))]
 pub fn derive(input: TokenStream) -> TokenStream {
     parse::expand(parse_macro_input!(input as DeriveInput))
--- src/parse.rs
+++ src/parse.rs
@@ -1,97 +1,97 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
 use proc_macro2::TokenStream as TokenStream2;
 
 use quote::{quote, ToTokens};
-use syn::*;
+use syn::{Fields, Meta, MetaNameValue, Expr, ExprLit, Lit, MetaList, DeriveInput, Data, DataStruct};
 
+/// Parse struct fields into an iterator over the
+/// doc comments of fields in the order of definition.
 fn parse_doc_comments_from_fields(fields: &Fields) -> impl Iterator<Item = String> + '_ {
-    fields.iter().map(|field| {
+    fields
+        .iter()
+        .map(|field| {
+
+        let mut doc_comments = vec![];
+
+        // Every individual doc comment is an attr.
         field
             .attrs
             .iter()
-            .find_map(|attr| {
+            .for_each(|attr| {
                 if let Meta::NameValue(MetaNameValue { path, value, .. }) = &attr.meta {
-                    if path.segments.iter().any(|segment| segment.ident == "doc") {
-                        // Some segment has "doc" so assume this is field has a doc comment.
-                        if let Expr::Lit(ExprLit {
-                            lit: Lit::Str(lit_str),
-                            ..
-                        }) = value
-                        {
-                            let mut raw_token = lit_str.token().to_string();
-                            if let Some(stripped) = raw_token.strip_prefix('\"') {
-                                raw_token = stripped.to_string();
+                    path
+                    .segments
+                    .iter()
+                    .for_each(|segment| {
+                        if segment.ident == "doc" {
+                            if let Expr::Lit(ExprLit {
+                                 lit: Lit::Str(lit_str),
+                             .. }) = value {
+                                let mut raw_token = lit_str.token().to_string();
+                                if let Some(stripped) = raw_token.strip_prefix('\"') {
+                                    raw_token = stripped.to_string();
+                                }
+                                if let Some(stripped) = raw_token.strip_suffix('\"') {
+                                    raw_token = stripped.to_string();
+                                }
+                                // Collect every line of doc-comment.
+                                doc_comments.push(raw_token.trim().to_string());
                             }
-                            if let Some(stripped) = raw_token.strip_suffix('\"') {
-                                raw_token = stripped.to_string();
-                            }
-                            return Some(raw_token.trim().to_string());
                         }
-                    }
+                    });
                 }
-                None
-            })
-            .unwrap_or("No doc comment found".to_string())
+            });
+
+        if doc_comments.is_empty() {
+            return "No doc comment found".to_string()
+        }
+        return doc_comments.join(" ");
     })
 }
 
+/// Parse fields for the widgets to generate from the `#[control]` field attributes.
 fn parse_widgets_from_fields(fields: &Fields) -> impl Iterator<Item = TokenStream2> + '_ {
-    let field_type = fields.iter().map(|field| &field.ty);
 
-    fields.iter().zip(field_type.into_iter())
-        .map(|(field, ftype)| {
-            let name = field.ident.clone().unwrap();
-            match ftype {
-                Type::Path(type_path)  => {
-                    let full_type = type_path.clone().into_token_stream().to_string();
-                    match full_type.as_str() {
-                        | "u8"
-                        | "u16"
-                        | "u32"
-                        | "u64"
-                        | "u128"
-                        | "usize"
-                        | "i8"
-                        | "i16"
-                        | "i32"
-                        | "i64"
-                        | "i128"
-                        | "isize"
-                        => {
-                            // FIXME: Change the hardcoded ranges to be configurable
-                            //        via #[control(slider(min=0, max=50))]
-                            quote!(
-                                eframe::egui::Slider::new(&mut state.#name, 0..=100)
-                            )
-                        },
-                        "f32" | "f64" => {
-                            // FIXME: Change the hardcoded ranges to be configurable
-                            //        via #[control(slider(min=0., max=2.0))]
-                            quote!(
-                                eframe::egui::Slider::new(&mut state.#name, 0.0..=1.0)
-                            )
-                        },
-                        _ => {
-                            // FIXME: How should we support other types?
-                            // For example, datetimes could be converted to a date picker.
-                            // Maybe allow a file upload marker as well.
-                            quote!(
-                                eframe::egui::TextEdit::singleline(&mut state.#name).hint_text("Unsupported type. Will treat it as string.")
-                            )
+    fields
+    .iter()
+    .flat_map(|field| {
+        let name = field.ident.clone().unwrap();
+        field
+        .attrs
+        .iter()
+        .filter_map(move |attr| {
+            if let Meta::List(MetaList { path, tokens, .. }) = &attr.meta {
+                if path.into_token_stream().to_string() == "control" {
+                    let mut token_iter = tokens.clone().into_iter();
+                    if let Some(proc_macro2::TokenTree::Ident(ident)) = token_iter.next() {
+                        if ident == "slider" {
+                            let proc_macro2::TokenTree::Group(group) =
+                                token_iter.next().expect("slider to be provided a InclusiveRange prop") else {
+                                panic!("slider expects an InclusiveRange prop.");
+                            };
+                            let stream = group.stream();
+                            return Some(quote!(
+                                ::eframe::egui::Slider::new(&mut self.#name, #stream)
+                            ))
                         }
+                        else if ident == "textbox" {
+                            return Some(quote!(
+                                ::eframe::egui::TextEdit::singleline(&mut self.#name).hint_text("")
+                            ))
+                        }
+                        return None;
                     }
-                },
-                _ => {
-                    // We probably don't care about non-path types
-                    // but might wanna handle the case.
-                    quote!(
-                        eframe::egui::TextEdit::singleline(&mut state.#name).hint_text("Unsupported type. Will treat it as string.")
-                    )
                 }
             }
+            None
         })
+    })
 }
 
+/// Expand the parsed struct into a [eframe::egui::Grid] of three columns
+/// where the first column is the struct field name, the second column
+/// is the interactive form control, and the third field is the description
+/// of the field extracted from the doc comment.
 pub fn expand(input: DeriveInput) -> TokenStream {
     let Data::Struct(DataStruct { fields, .. }) = &input.data else {
         panic!("expected a struct");
@@ -100,45 +100,33 @@ pub fn expand(input: DeriveInput) -> TokenStream {
     let struct_name = &input.ident;
     let grid_id = format!("{}__control-panel", input.ident);
 
-    let field_name = fields.iter().map(|field| &field.ident);
-    let field_doc = parse_doc_comments_from_fields(fields);
-    let field_widget = parse_widgets_from_fields(fields);
+    let field_names = fields.iter().map(|field| &field.ident);
+    let field_docs = parse_doc_comments_from_fields(fields);
+    let field_widgets = parse_widgets_from_fields(fields);
 
+    let setting_heading = "Setting";
+    let value_heading = "Value";
+    let description_heading = "Description";
+    
     let expanded = quote! {
 
-        impl ::eframe::egui::Widget for #struct_name {
-            fn ui(self, ui: &mut ::eframe::egui::Ui) -> ::eframe::egui::Response {
+        impl #struct_name {
+            fn ui(&mut self, ui: &mut ::eframe::egui::Ui) -> ::eframe::egui::Response {
 
-                let id = ui.id();
-                let state = ui.memory_mut(|mem| {
-                    mem.data
-                    .get_persisted_mut_or_insert_with(id, || std::sync::Arc::new(std::sync::Mutex::new(#struct_name::default())))
-                    .clone()
-                });
-
-                let mut state =
-                    state
-                    .lock()
-                    .unwrap();
-
-                egui::Grid::new(#grid_id)
+                ::eframe::egui::Grid::new(#grid_id)
                 .num_columns(3)
                 .spacing([40., 20.])
                 .striped(true)
                 .show(ui, |ui| {
-                    ui.heading("Setting");
-                    ui.heading("Value");
-                    ui.heading("Description");
-
+                    ui.heading(#setting_heading);
+                    ui.heading(#value_heading);
+                    ui.heading(#description_heading);
                     ui.end_row();
-
                     #(
                         {
-                            ui.label(stringify!(#field_name));
-                            ui.add(#field_widget);
-                            ui.vertical(|ui| {
-                                ui.label(#field_doc);
-                            });
+                            ui.label(stringify!(#field_names));
+                            ui.add(#field_widgets);
+                            ui.vertical(|ui| ui.label(#field_docs));
                             ui.end_row();
                         }
                     )*
